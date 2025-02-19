7c898c3a74aba89d0627abe3a382659ab8e53a06
yeabnoah <yeabnoah5@gmail.com>
2025-02-19 22:19:15 +0300

    landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,17 +4,21 @@ import { Button } from "@/components/ui/button";
 import useSecretData from "@/store/secret.store";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import ThemeSwitcher from "@/components/theme-switcher";
 
 export default function Home() {
   const { isAiFetching } = useSecretData();
   const router = useRouter()
   return (
-    <main className="bg-bgMain min-h-screen text-white dark:bg-bgMain dark:text-white">
+    <main className=" min-h-screen bg-white/50 text-bgMain  dark:bg-bgMain dark:text-white">
       <div className="container mx-auto">
+        <div className="absolute top-4 right-4">
+          <ThemeSwitcher />
+        </div>
 
-        <div className="flex flex-col pt-[15vh] sm:pt-[23vh] items-center justify-center min-h-[80vh] text-center gap-4 sm:gap-6 px-4">
+        <div className="flex flex-col pt-[15vh] sm:pt-[23vh] items-center justify-center min-h-[80vh] text-center gap-4 sm:gap-6black px-4">
 
-          <h1 className="text-4xl font-inter sm:text-6xl md:text-7xl font-bold max-w-4xl leading-tight text-black dark:text-white">
+          <h1 className="text-4xl font-inter sm:text-6xl md:text-7xl font-bold max-w-4xl leading-tight text-bgMain dark:text-white">
             Share your <span className="font-instrument italic">Secrets</span> {" "}
             <span className="block">
               in <span className="font-instrument italic">Seconds</span>.
@@ -27,7 +31,7 @@ export default function Home() {
 
           <Button
             onClick={() => router.push("/dashboard")}
-            className="px-6 sm:px-8 py-4 sm:py-6 font-instrument text-xl sm:text-2xl bg-black dark:bg-white text-white dark:text-black hover:bg-black/90 dark:hover:bg-white/90 transition-all duration-200 flex items-center gap-2"
+            className="px-6 sm:px-8 py-4 sm:py-6 font-instrument text-xl sm:text-2xl bg-bgMain dark:bg-white text-white dark:text-bgMain hover:bg-bgMain/90 dark:hover:bg-white/90 transition-all duration-200 flex items-center gap-2"
           >
             {isAiFetching ? (
               <>
